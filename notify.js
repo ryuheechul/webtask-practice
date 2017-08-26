0b66852d7ea623d598edac3716b906cc517ad181
Heechul Ryu <ryuhcii@gmail.com>
2017-08-26 15:34:05 +0900

    Create notify.js

---
+++ notify.js
@@ -0,0 +1,57 @@
+'use latest';
+
+import express from 'express';
+import { fromExpress } from 'webtask-tools';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
+const app = express();
+
+const getTransporter = (ctx) => {
+  return nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: ctx.secrets.GMAIL_ID,
+      pass: ctx.secrets.GMAIL_PW
+    }
+  });
+};
+
+const onlyUser = 'ryuhcii@gmail.com';
+
+const html = `
+<h1>Let\'s preview and plan your productive day!</h1>
+Open your <a href="https://calendar.google.com">Google Calendar</a> to do it!
+`;
+
+const getMailOptions = (ctx) => {
+  return {
+    from: "Robin <" + ctx.secrets.GMAIL_ID + ">",
+    to: onlyUser,
+    subject: 'Preview your day!',
+    html: html
+  }
+};
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  const ctx = req.webtaskContext;
+  const smtpTransport = getTransporter(ctx);
+  const mailOptions = getMailOptions(ctx);
+  
+  res.set('Content-Type', 'text/html');
+  
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if (error){
+      console.log(error);
+      res.status(401).send('failed to login');
+    } else {
+      console.log("Message sent : " + response.message);
+      res.set('Content-Type', 'text/html');
+      res.status(200).send('success');
+    }
+    smtpTransport.close();
+  });
+});
+
+module.exports = fromExpress(app);
